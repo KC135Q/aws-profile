e74abf6a88204ccc95b96736e490098f58ff8adc
Dan K <KC135Q@users.noreply.github.com>
2019-06-09 11:44:00 -0400

    User model created, passport added, now need to add register option

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # aws-profile
 Basic layout for an aws profile
+
+## Getting started
+* Create database according to `config->config.json` file
+* Update environment variables for
+  * 
+* `npm i` to update node modules
+* `npm run start` to run application
+* Open application in terminal
+
+### AWS Reference
+* [S3 console](https://s3.console.aws.amazon.com/s3/home?region=us-east-2#)
+* [AWS-SDK npm](https://www.npmjs.com/package/aws-sdk) - more on [github](https://github.com/aws/aws-sdk-js/blob/master/SERVICES.md)
+
+### Passport Reference
+* [http://www.passportjs.org/docs/username-password/](http://www.passportjs.org/docs/username-password/)
+* [User Model Definition](https://code.tutsplus.com/tutorials/using-passport-with-sequelize-and-mysql--cms-27537) - just change `user` to `User` on line 03
--- server.js
+++ server.js
@@ -7,10 +7,29 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    db.User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
 
 // Handlebars
 app.engine(
@@ -22,6 +41,14 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
